f44f3fbfd790fbc0f745cc39b22fdfa27e3dfcdf
ConleyLee <lika52090@gmail.com>
2019-04-22 00:52:15 +0800

    重新规划训练代码，可直接配套训练数据集使用

--- Model/SentencePresentation.py
+++ Model/SentencePresentation.py
@@ -9,9 +9,10 @@ else:
     from .SelfAttention import SelfAttention
     from .Dataset import Dataset
 
+
 class SentencePresentation(object):
-    def __init__(self, wv, lstm_size=128, layers=2, wv_dim=100, dim_a=200, dim_r=50, 
-                    alpha=1.0, lr=0.001, norm=5.0, drop_out=0.5, classes=2):
+    def __init__(self, wv, lstm_size=128, layers=2, wv_dim=100, dim_a=200, dim_r=50,
+                 alpha=1.0, lr=0.001, norm=5.0, drop_out=0.5, classes=2):
         x, lens, vocab, dropout, output = BiLSTM(lstm_size=lstm_size, layers=layers, wv_dim=wv_dim).io_nodes()
         self._x = x
         self._lens = lens
@@ -27,9 +28,9 @@ class SentencePresentation(object):
         self._dr = drop_out
         self._classes = classes
         self._wv = wv
-        self._build_graph()                
+        self._build_graph()
 
-    def _build_graph(self):            
+    def _build_graph(self):
         self._y = tf.placeholder(dtype=tf.int64, shape=[None], name='input-y')
         self._mask = tf.sequence_mask(self._lens, dtype=tf.float64)
 
@@ -41,36 +42,36 @@ class SentencePresentation(object):
             #self._fc = slim.fully_connected(sentence_embedding, 500, activation_fn=tf.nn.relu)
             self._fc = slim.fully_connected(self._sentence_embedding, self._classes, activation_fn=None)
             self._pre = tf.nn.softmax(self._fc)
-        
+
         with tf.name_scope('penalization'):
             AA_T = tf.matmul(self._attention, tf.transpose(self._attention, [0, 2, 1]))
             cur_batch = tf.shape(self._x)[0]
             I = tf.eye(self._dim_r, batch_shape=[cur_batch], dtype=tf.float64)
             P = tf.square(tf.norm(AA_T - I, axis=[1, 2], ord='fro'))
-        
+
         with tf.name_scope('loss'):
             loss = tf.nn.sparse_softmax_cross_entropy_with_logits(logits=self._fc, labels=self._y)
-            
+
             self._loss = tf.reduce_mean(loss) + tf.reduce_mean(self._alpha*P)
             optimizer = tf.train.AdamOptimizer(learning_rate=self._lr)
             optimizer = tf.contrib.estimator.clip_gradients_by_norm(optimizer, clip_norm=self._norm)
             optimizer = tf.contrib.estimator.clip_gradients_by_norm(optimizer, clip_norm=-self._norm)
             self._train_op = optimizer.minimize(self._loss)
-        
+
         with tf.name_scope('acc'):
             pre = tf.argmax(self._fc, axis=1)
             acc = tf.equal(pre, self._y)
             self._acc = tf.reduce_mean(tf.cast(acc, tf.float64))
 
-    def fit(self, sess, datapath,batch_size=50, epoch=5, max_len=500):            
+    def fit(self, sess, datapath, batch_size=50, epoch=5, max_len=500):
         dataset = Dataset(sess, datapath, batch_size, '\t', max_len=max_len, epoch=epoch)
         sess.run(tf.initialize_all_variables())
         for steps, (c, ws, lens) in enumerate(dataset):
-            feed = {self._x : ws, self._y: c, self._lens: lens, self._vocab: self._wv, self._dropout: self._dr}
+            feed = {self._x: ws, self._y: c, self._lens: lens, self._vocab: self._wv, self._dropout: self._dr}
             loss, acc, _ = sess.run([self._loss, self._acc, self._train_op], feed_dict=feed)
             yield steps, loss, acc
 
     def predict(self, sess, x, lens):
-        feed = {self._x : x, self._vocab: self._wv, self._dropout: 1.0, self._lens: lens}
+        feed = {self._x: x, self._vocab: self._wv, self._dropout: 1.0, self._lens: lens}
         labels, attentions, embedding = sess.run([self._pre, self._attention, self._sentence_embedding], feed_dict=feed)
         return labels, attentions, embedding
--- main.py
+++ main.py
@@ -3,13 +3,17 @@ from Model import SentencePresentation
 import numpy as np
 from gensim.models import word2vec
 import json
+from Model.Dataset import Dataset
 
-if __name__ == "__main__":    
+if __name__ == "__main__":
     model = word2vec.Word2Vec.load('./wv.gensim.model')
     wv = model.wv.vectors
     network =SentencePresentation(wv, wv_dim=100, lstm_size=64, layers=1, dim_r=30, classes=4, dim_a=10, norm=0.5, lr=0.01)
     with tf.Session() as sess:
-        network.fit(sess, './train_data.csv', epoch=2)
+        sess.run(tf.initialize_all_variables())
+        for steps, loss, acc in network.fit(sess, './train_data.csv', epoch=2):
+            print('steps: %d, loss: %f, acc: %f' % (steps, loss, acc))
+
         dataset = Dataset(sess, './train_data.csv', 200, '\t', max_len=500, epoch=1)
         for c, ws, lens in dataset:
             labels, attentions, embedding = network.predict(sess, ws, lens)
@@ -18,7 +22,7 @@ if __name__ == "__main__":
                 w = [model.wv.index2word[i] for i in w]
                 at = np.max(attention, axis=0)
                 for c, a in zip(w, at):
-                    words_json.append({'word': c, 'attention': a}) 
+                    words_json.append({'word': c, 'attention': a})
                 words_json.append({'word': '\n', 'attention': 0.0})
                 print('\n\n')
                 print(json.dumps(words_json, ensure_ascii=False))
