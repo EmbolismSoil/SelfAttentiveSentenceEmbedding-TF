f0fc911d09ae60039d9e7891c572252205a396e3
ConleyLee <lika52090@gmail.com>
2019-04-22 00:40:50 +0800

    merge origin

--- Model/BiLSTM.py
+++ Model/BiLSTM.py
@@ -1,6 +1,7 @@
 import tensorflow as tf
 from tensorflow.nn.rnn_cell import BasicLSTMCell, DropoutWrapper, MultiRNNCell
 
+
 class BiLSTM(object):
     def __init__(self, lstm_size=128, layers=2, wv_dim=100):
         self._lstm_size = lstm_size
@@ -14,22 +15,24 @@ class BiLSTM(object):
             self._x = tf.placeholder(dtype=tf.int32, shape=[None, None], name='input-x')
             self._vocab = tf.placeholder(dtype=tf.float64, shape=[None, self._wv_dim], name='input-word-vector')
             embedded_x = tf.nn.embedding_lookup(self._vocab, self._x)
-            
 
         with tf.name_scope('multi-bilstm-layer'):
             self._drop_out_placeholder = tf.placeholder(tf.float64, name='drop_out')
-            fw_cells = [DropoutWrapper(BasicLSTMCell(self._lstm_size, state_is_tuple=True), output_keep_prob=self._drop_out_placeholder) for _ in range(self._layers)]
-            bw_cells = [DropoutWrapper(BasicLSTMCell(self._lstm_size, state_is_tuple=True), output_keep_prob=self._drop_out_placeholder) for _ in range(self._layers)]
+            fw_cells = [DropoutWrapper(BasicLSTMCell(self._lstm_size, state_is_tuple=True),
+                                       output_keep_prob=self._drop_out_placeholder) for _ in range(self._layers)]
+            bw_cells = [DropoutWrapper(BasicLSTMCell(self._lstm_size, state_is_tuple=True),
+                                       output_keep_prob=self._drop_out_placeholder) for _ in range(self._layers)]
             self._fw_cell = MultiRNNCell(fw_cells)
             self._bw_cell = MultiRNNCell(bw_cells)
 
-            outputs, states = tf.nn.bidirectional_dynamic_rnn(self._fw_cell, self._bw_cell, embedded_x, sequence_length=self._lens, dtype=tf.float64)
+            outputs, states = tf.nn.bidirectional_dynamic_rnn(
+                self._fw_cell, self._bw_cell, embedded_x, sequence_length=self._lens, dtype=tf.float64)
             output = tf.concat(outputs, axis=2)
             self._output = output
 
     def io_nodes(self):
         return self._x, self._lens, self._vocab, self._drop_out_placeholder, self._output
-        
-        
+
+
 if __name__ == "__main__":
     lstm = BiLSTM()
